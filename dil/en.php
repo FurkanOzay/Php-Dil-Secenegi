8f267b69625eece6fe90b2cf1f96bea033018118
Furkan Özay <furkanozay0@gmail.com>
2021-12-19 15:35:19 +0300

    Dil seçeneği dosyaları oluşturldu

---
+++ dil/en.php
@@ -0,0 +1,19 @@
+<?php 
+
+    #İngilizce 
+
+    $dil = array(
+        "baslik"    => "Language Option Page",
+        "trdil"     => "Turkish",
+        "ingdil"    => "English",
+        "ardil"     =>  "Arabic",
+        "rusdil"    => "Russian",
+        "menu1"     => "Home",
+        "menu2"     => "About",
+        "menu3"     => "Services",
+        "ds"        => "Lang Options",
+        "p"         => "The text here is a sample text. If you don't want language change only in the menu area, you can manually set the elements on your site in the language folder. In this way, you will be able to translate the elements you specify into the language you want."
+
+    )
+
+?>
---
+++ dil/tr.php
@@ -0,0 +1,18 @@
+<?php 
+
+    #İngilizce 
+    
+
+    $dil = array(
+        "baslik"    => "Dil Seçeneği Sayfası",
+        "trdil"     => "Türkçe",
+        "ingdil"    => "İngilizce",
+        "ardil"     => "Arapça",
+        "rusdil"    => "Rusça",
+        "menu1"     => "Ana Sayfa",
+        "menu2"     => "Hakkımızda",
+        "menu3"     => "Hizmetlerimiz",
+        "ds"        => "Dil Seçeneği",
+        "p"         => "Buradaki metin örnek bir metindir. Sadece menü alanında dil değişikliği istemiyorsanız sitenizdeki elementleri dil klasörü içerisinde manuel olarak ayarlayabilirsiniz. Böyle belirlediğiniz elementleri istediğiniz dile çevirebileceksiniz."
+    )
+?>
